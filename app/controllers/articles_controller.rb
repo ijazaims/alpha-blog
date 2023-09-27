58c15a3691a432b07dfe59e11b8ea2ebc06cc9c3
ijazaims <ijazaims@hotmail.com>
2023-09-27 10:47:11 +0500

    Add edit article & update feature

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+    def show
+        # byebug
+        # if Article.find(pa rams[:id]).nil?
+            @article = Article.find(params[:id]) 
+        # end
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id]) 
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        # render plain: params[:article]
+        # render plain: @article.inspect
+        if @article.save
+            flash[:notice] = "Article is created successfully."
+            flash.keep(:notice) #keep the flash entry available for the next action
+            # redirect_to article_path(@article)    #articles/is
+            redirect_to @article                  #same as above
+            # redirect_to articles_path             #show all - index
+            # redirect_to @articles                 #not working
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article is updated successfully."
+            flash.keep(:notice) #keep the flash entry available for the next action
+            # redirect_to article_path(@article)    #articles/is
+            redirect_to @article                  #same as above
+            # redirect_to articles_path             #show all - index
+            # redirect_to @articles                 #not working
+        else
+            render 'edit'
+        end
+    end
+end
